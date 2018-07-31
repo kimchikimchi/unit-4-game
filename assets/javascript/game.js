ae507823ef1d310bfad4166d761bba1bbf5127cb
Jay Cha <kimchikimchi@gmail.com>
2018-07-30 21:41:46 -0400

    Wrap the whole javascript into $(document).ready function so that it only executes after the page is fully loaded

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,71 +9,75 @@
 - Game restarts whenever the player wins or loses.
 */
 
-var gameData = {
-    randomNumber: undefined,
-    totalScore: 0,
-    wins: 0,
-    losses: 0,
-    red: undefined,
-    blue: undefined,
-    yellow: undefined,
-    green: undefined,
-    displayMsg: "",
-};
+$(document).ready( function() {
+
+    var gameData = {
+        randomNumber: undefined,
+        totalScore: 0,
+        wins: 0,
+        losses: 0,
+        red: undefined,
+        blue: undefined,
+        yellow: undefined,
+        green: undefined,
+        displayMsg: "",
+    };
 
 
-function initializeGame() {
-    // Generates number between 19 and 120
-    gameData.randomNumber = Math.floor( Math.random() * 102) + 19;
-    gameData.red = Math.floor( Math.random() * 12) + 1;
-    gameData.blue = Math.floor( Math.random() * 12) + 1;
-    gameData.yellow = Math.floor( Math.random() * 12) + 1;
-    gameData.green = Math.floor( Math.random() * 12) + 1;
-    gameData.totalScore = 0;
+    function initializeGame() {
+        // Generates number between 19 and 120
+        gameData.randomNumber = Math.floor( Math.random() * 102) + 19;
+        gameData.red = Math.floor( Math.random() * 12) + 1;
+        gameData.blue = Math.floor( Math.random() * 12) + 1;
+        gameData.yellow = Math.floor( Math.random() * 12) + 1;
+        gameData.green = Math.floor( Math.random() * 12) + 1;
+        gameData.totalScore = 0;
 
-    console.log("Starting a new game: ");
-    console.log(gameData);
+        console.log("Starting a new game: ");
+        console.log(gameData);
 
-    drawBoard();
-}
-
-function drawBoard() {
-    $("#displayMsg").text(gameData.displayMsg);
-    $("#randomNumber").text(gameData.randomNumber);
-    $("#totalScore").text(gameData.totalScore);
-    $("#wins").text(gameData.wins);
-    $("#losses").text(gameData.losses);
-}
-
-function addGemValue(color) {
-    console.log(`Adding ${color} :  ${ gameData[color] }`)
-    gameData.totalScore += parseInt(gameData[color]);
-}
-
-function isWinOrLose() {
-    if ( gameData.randomNumber === gameData.totalScore ) {
-        gameData.wins++;
-        //alert("You WON this round");
-        gameData.displayMsg = "You WON!";
-        return true;
-    } else if (gameData.randomNumber < gameData.totalScore) {
-        gameData.losses++;
-        //alert("You LOST this round");
-        gameData.displayMsg = "You LOST!";
-        return true;
-    } else {
-        return false;
+        drawBoard();
     }
-}
 
-$(".gemButton").on('click', function() {
-    console.log(`Pressed ${this.value}`);
-    addGemValue(this.value);
-    drawBoard();
-
-    if ( isWinOrLose() ) {
-        initializeGame();
+    function drawBoard() {
+        $("#displayMsg").text(gameData.displayMsg);
+        $("#randomNumber").text(gameData.randomNumber);
+        $("#totalScore").text(gameData.totalScore);
+        $("#wins").text(gameData.wins);
+        $("#losses").text(gameData.losses);
     }
+
+    function addGemValue(color) {
+        console.log(`Adding ${color} :  ${ gameData[color] }`)
+        gameData.totalScore += parseInt(gameData[color]);
+    }
+
+    function isWinOrLose() {
+        if ( gameData.randomNumber === gameData.totalScore ) {
+            gameData.wins++;
+            //alert("You WON this round");
+            gameData.displayMsg = "You WON!";
+            return true;
+        } else if (gameData.randomNumber < gameData.totalScore) {
+            gameData.losses++;
+            //alert("You LOST this round");
+            gameData.displayMsg = "You LOST!";
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    $(".gemButton").on('click', function() {
+        console.log(`Pressed ${this.value}`);
+        addGemValue(this.value);
+        drawBoard();
+
+        if ( isWinOrLose() ) {
+            initializeGame();
+        }
+    });
+
+    initializeGame();
+
 });
-
-initializeGame();
